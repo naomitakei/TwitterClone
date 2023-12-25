3db763e7f94e849c674362ff77e017819e2782a5
naomitakei <cherryblossom.nao1130@gmail.com>
2023-12-25 18:07:58 +0900

    会員登録画面＆ログイン画面

--- Views/home.php
+++ Views/home.php
@@ -103,7 +103,13 @@ $view_tweets = [
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php echo HOME_URL; ?>Views/css/style.css">
-     
+        <!-- JS -->
+         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+        <!-- いいね！JS -->
+        <script src="<?php echo HOME_URL; ?>Views/js/likes.js" defer></script>
+
         <title>ホーム画面 / Twitterクローン</title>
         <meta name="description" content="ホーム画面です">
     </head>
@@ -175,7 +181,7 @@ $view_tweets = [
                                     <?php endif; ?>
      
                                     <div class="icon-list">
-                                        <div class="like">
+                                        <div class="like js-like" date-like-id="<?php echo htmlspecialchars($view_tweet['like_id']);?> ">
                                             <?php
                                             if (isset($view_tweet['like_id'])) {
                                                 // いいね！している場合、青のハートを表示
@@ -186,7 +192,7 @@ $view_tweets = [
                                             }
                                             ?>
                                         </div>
-                                        <div class="like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
+                                        <div class="like-count js-like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
                                     </div>
                                 </div>
                             </div>
